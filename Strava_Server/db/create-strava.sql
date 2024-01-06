5516131ac1839b5f89ce8d4adc7674a4663222f4
jonlaasa <113533659+jonlaasa@users.noreply.github.com>
2024-01-06 21:47:22 +0100

    Update create-strava.sql

--- Strava_Server/db/create-strava.sql
+++ Strava_Server/db/create-strava.sql
@@ -1,10 +1,11 @@
-/* DELETE 'stravadb' database*/
+/* DELETE 'strava_user' database*/
 DROP SCHEMA IF EXISTS stravadb;
-/* DELETE USER 'strava_user' AT LOCAL SERVER*/
+/* DELETE USER 'auctions_user' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'strava_user'@'%';
+
 /* CREATE ''stravadb' DATABASE */
 CREATE SCHEMA IF NOT EXISTS stravadb;
-/* CREATE THE USER 'strava_user' AT LOCAL SERVER WITH PASSWORD 'YES' */
-CREATE USER IF NOT EXISTS 'strava_user'@'%' IDENTIFIED BY 'YES';
-/* GRANT FULL ACCESS TO THE DATABASE FOR THE USER 'strava_user' AT LOCAL SERVER*/
+/* CREATE THE USER 'strava_user' AT LOCAL SERVER WITH PASSWORD 'strava_user' */
+CREATE USER IF NOT EXISTS 'strava_user'@'%' IDENTIFIED BY 'password';
+/* GRANT FULL ACCESS TO THE DATABASE 'stravadb' FOR THE USER 'strava_user' AT LOCAL SERVER*/
 GRANT ALL ON stravadb.* TO 'strava_user'@'%';
